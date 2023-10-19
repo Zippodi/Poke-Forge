28d0f759e9c3a065ea0b49ac51126fe408ada497
Matthew Welker <mdwelker@ncsu.edu>
2023-10-19 00:42:52 -0400

    added mock user in authentication middleware

--- Milestone1/api/src/authentication/auth-middleware.js
+++ Milestone1/api/src/authentication/auth-middleware.js
@@ -1,12 +1,12 @@
 const exress = require('express');
 
 exports.auth = (req, res, next) => {
-  let authenticated = true; //authenticate user - change to false to test redirect
+  let authenticated = true; //authenticate user
   if (!authenticated) {
-    // return res.redirect(303, "/api/auth/login");
+    // return res.redirect(303, "/api/auth/login"); //redirect to login page
     return res.status(401).json({ "authenticated": false });
   } else {
-    req.user = { id: 1, username: "testuser" };
+    req.user = { id: 1, username: "testuser" }; //test user information
   }
   next();
 }
