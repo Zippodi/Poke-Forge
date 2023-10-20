61e73a51e82533a15625064fb31277274cd4c8f5
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-20 19:07:31 -0400

    Added frontend functionality to most pages (styling)

--- Milestone1/frontend/src/static/js/createscript.js
+++ Milestone1/frontend/src/static/js/createscript.js
@@ -1,3 +1,15 @@
+function insertAfter(referenceNode, newNode) {
+    referenceNode.parentNode.insertBefore(newNode, referenceNode.nextSibling);
+}
+
+function addPokemonSprite(node) {
+    let dateSpan = document.createElement('span')
+    let stringName = "pokesprite pokemon ";
+    dateSpan.className = stringName.concat("pikachu");
+
+    insertAfter(node, dateSpan);
+}
+
 class HTTPClient {
     static get(url) {
       return fetch(url).then(res => {
@@ -16,11 +28,23 @@ HTTPClient.get('/api/pokemon').then(pokemon => {
     var list = document.getElementById('pokemonlist');
     for (key in pokemon) {
         let option = document.createElement('option');
-            option.value = key;
+        
+            let name = key[0].toUpperCase() + key.slice(1);
+            option.value = name;
             list.appendChild(option);
     }
 
 });
+
+
+
+// let btn = document.getElementById("mah");
+//     btn.addEventListener("input", (e) => {
+//     // if (btn.textContent == "Pikachu") {
+//     //     addPokemonSprite(document.getElementById("mah"));
+//     // }
+//     console.log(btn.value);
+// });
 
 
   
