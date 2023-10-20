00ec5d0bd988307d5fe63467bfc97a6d4630882d
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-20 19:42:45 -0400

    Trying to set the entrypoint to Login.html

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -7,5 +7,10 @@ const PORT = process.env.PORT;
 app.use(express.static(__dirname + '/static'));
 
 
+// app.get('/', (req, res) => {
+//     res.sendFile(html_path + 'login.html');
+//   });
+
+
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
