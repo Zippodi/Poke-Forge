0a67da34e3a087d564975e557127cbea4638d2c3
Casey Dockery <cbdocke2@ncsu.edu>
2023-10-20 21:56:16 -0400

    Cleaned up some of the html pages and added scripting for going from page to page

--- Milestone1/frontend/src/app.js
+++ Milestone1/frontend/src/app.js
@@ -19,5 +19,13 @@ app.get('/register', (req, res) => {
   res.sendFile(getFile('register.html'));
 });
 
+app.get('/home', (req, res) => {
+  res.sendFile(getFile('home.html'));
+});
+
+app.get('/create', (req, res) => {
+  res.sendFile(getFile('createnewteam.html'));
+});
+
 // As our server to listen for incoming connections
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
--- Milestone1/frontend/src/static/js/loginscript.js
+++ Milestone1/frontend/src/static/js/loginscript.js
@@ -1,5 +1,5 @@
 addEventListener('DOMContentLoaded', (e) => {
-    let btn = document.querySelector("#Send");
+    let btn = document.querySelector("#registerbutton");
     btn.addEventListener('click', (e) => {
         fetch('/api/auth/login', {
             method: "POST",
@@ -10,6 +10,24 @@ addEventListener('DOMContentLoaded', (e) => {
             body: JSON.stringify()
         }).then((res) => {
             window.location.href = '/register';
+        }).catch(err => {
+            console.error(err);
+        });
+    });
+});
+
+addEventListener('DOMContentLoaded', (e) => {
+    let btn = document.querySelector("#Send");
+    btn.addEventListener('click', (e) => {
+        fetch('/api/auth/login', {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify()
+        }).then((res) => {
+            window.location.href = '/home';
         }).catch(err => {
             console.error(err);
         });
