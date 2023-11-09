1c5f58ac82dc200aff11110e4cf59024a90058ed
Casey Dockery <cbdocke2@ncsu.edu>
2023-11-09 18:42:55 -0500

    Added more script files and added middleware. Styled register.

--- Milestone2/frontend/src/static/js/loginscript.js
+++ Milestone2/frontend/src/static/js/loginscript.js
@@ -1,20 +1,33 @@
+// addEventListener('DOMContentLoaded', (e) => {
+//     let btn = document.querySelector("#registerbutton");
+//     btn.addEventListener('click', (e) => {
+//         fetch('/api/auth/login', {
+//             method: "POST",
+//             headers: {
+//                 'Accept': 'application/json',
+//                 'Content-Type': 'application/json'
+//             },
+//             body: JSON.stringify()
+//         }).then((res) => {
+//             window.location.href = '/register';
+//         }).catch(err => {
+//             console.error(err);
+//         });
+//     });
+// });
+
 addEventListener('DOMContentLoaded', (e) => {
-    let btn = document.querySelector("#registerbutton");
-    btn.addEventListener('click', (e) => {
-        fetch('/api/auth/login', {
-            method: "POST",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify()
-        }).then((res) => {
-            window.location.href = '/register';
-        }).catch(err => {
-            console.error(err);
-        });
-    });
+    let buttonSelect = document.querySelector("#registerbutton");
+    buttonSelect.addEventListener('click', e => {
+       
+          
+        
+          
+        document.location = "/register";
+    });   
+
 });
+
 
 addEventListener('DOMContentLoaded', (e) => {
     let btn = document.querySelector("#Send");
