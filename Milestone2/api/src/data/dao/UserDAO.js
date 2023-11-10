15a4c27be712d11d9c4211add0237418ab3523d3
Casey Dockery <cbdocke2@ncsu.edu>
2023-11-10 00:11:13 -0500

    Registering user almost complete

--- Milestone2/api/src/data/dao/UserDAO.js
+++ Milestone2/api/src/data/dao/UserDAO.js
@@ -16,35 +16,32 @@ function getUserByCredentials(username, password) {
 }
 
 function createUser(username, password) {
-  return new Promise((resolve, reject) => {
-    if (!validateTeamBody(team)) {
-      reject('Invalid team body');
-    }
-    let connection = db.getDatabaseConnection();
-    connection.getConnection((err, conn) => {
-      conn.beginTransaction(async (err) => {
-        if (err) {
-          reject(err);
+      //insert User
+      let salt = crypto.randomBytes(32);
+      
+      salt = salt.toString('hex');
+      crypto.pbkdf2(password, salt, 100000, 64, 'sha512', (err, derivedKey) => {
+        if (err) { //problem computing digest, like hash function not available
+          reject("Error: " +err);
         }
-        //insert User
-        let salt = crypto.randomBytes(64);
-        salt = salt.toString('hex');
-        let hashedPassword = hashPassword(password, salt);
-        conn.query('INSERT INTO team(username, password, salt) VALUES (?, ?, ?)', [username, hashedPassword, salt], async (err, results, fields) => {
-          if (err) {
-            return conn.rollback(() => { reject(err); });
+    
+        const hashedPassword = derivedKey.toString('hex');
+        return db.query('INSERT INTO user(username, password, salt) VALUES (?, ?, ?)', [username, hashedPassword, salt]).then(({results}) => {
+          const user = new User(results[0]);
+          if (user) { // we found our user
+            return user.validatePassword(password);
           }
-          const newUserId = results.insertId;
-          conn.commit((err) => {
-            if (err) {
-              return conn.rollback(() => { reject(err); });
-            }
-            resolve(newUserId);
-          });
+          else { // if no user with provided username
+            throw new Error("No such user");
+          }
         });
+        
+            
+          
+          
       });
-    });
-  });
+
+    
 }
 
 
